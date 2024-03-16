62f205afa5da186e0e2d5fa0bded2947edbae019
mikic202 <mikolaj.chomanski@gmail.com>
2024-03-16 09:07:59 +0100

    update motor class to acomodate new motor controller type

--- HardwareClasses/Motor.py
+++ HardwareClasses/Motor.py
@@ -3,29 +3,25 @@ from gpiozero import PWMOutputDevice, DigitalOutputDevice
 
 class Motor:
     def __init__(
-        self, first_enable_pin: int, second_enable_pin: int, pwm_pin: int, max_pwm: float = 0.9
+        self, logic_input_1: int, logic_input_2: int, max_pwm: float = 0.9
     ) -> None:
-        self.first_enable_pin = first_enable_pin
-        self.second_enable_pin = second_enable_pin
-        self.pwm_pin = pwm_pin
-        self._pwm_output = PWMOutputDevice(pwm_pin)
-        self._second_enable_output = DigitalOutputDevice(second_enable_pin)
-        self._first_enable_output = DigitalOutputDevice(first_enable_pin)
+        self.logic_input_1 = logic_input_1
+        self.logic_input_2 = logic_input_2
+        self._pwm_output = PWMOutputDevice(logic_input_1)
+        self._enable_pin = DigitalOutputDevice(logic_input_2)
         self.reset()
         self.max_pwm = max_pwm
 
     def reset(self):
-        self._first_enable_output.off()
-        self._second_enable_output.off()
+        self._enable_pin.off()
+        self._pwm_output.off()
 
     def set_pwm(self, pwm_value: float):
         if pwm_value < 0.4:
             self.reset()
         if pwm_value > 0:
-            self._first_enable_output.on()
-            self._second_enable_output.off()
+            self._enable_pin.off()
+            self._pwm_output.value = min(abs(pwm_value), self.max_pwm)
         else:
-            self._first_enable_output.off()
-            self._second_enable_output.on()
-        self._pwm_output.value = min(abs(pwm_value), self.max_pwm)
-        print(self._pwm_output.value)
+            self._enable_pin.off()
+            self._pwm_output.value = min(1-abs(pwm_value), self.max_pwm)
