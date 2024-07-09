6b5b9649449f69f506a8fa55f1e26b3263921e4f
mikic202 <mikolaj.chomanski@gmail.com>
2024-07-09 20:52:22 +0200

    Fix sensor array to enable multiple sensors

--- HardwareClasses/DistanceSensor.py
+++ HardwareClasses/DistanceSensor.py
@@ -6,18 +6,19 @@ import adafruit_vl53l0x
 
 
 class DistanceSensor:
-    def __init__(self, i2c_bus: busio.I2C, off_pin: int) -> None:
+    def __init__(self, i2c_bus: busio.I2C, off_pin: DigitalOutputDevice) -> None:
+        self._off_pin = off_pin
+        self._off_pin.on()
+        time.sleep(0.5)
         self._i2c_bus = i2c_bus
-        self._off_pin = DigitalOutputDevice(off_pin)
         self._vl53l0x = adafruit_vl53l0x.VL53L0X(self._i2c_bus)
-        self._off_pin.off()
 
     def __call__(self) -> float:
         return self._vl53l0x.range
 
     def set_address(self, addres: int):
         self._off_pin.on()
-        time.sleep(1.0)
+        time.sleep(0.5)
         self._vl53l0x.set_address(addres)
 
     def off(self):
--- HardwareClasses/SensorArray.py
+++ HardwareClasses/SensorArray.py
@@ -1,24 +1,27 @@
 from HardwareClasses.DistanceSensor import DistanceSensor
+from gpiozero import DigitalOutputDevice
 from typing import List
 import time
 import board
 from adafruit_vl53l0x import VL53L0X
 
 class SensorArray:
-    def __init__(self, sensor_list: List[DistanceSensor]):
-        self._sensors_list = sensor_list
-        self._init_sensors()
+    def __init__(self, sensor_pins_list: List[int]):
+        self._sensors_list = []
+        self._i2c = board.I2C()
+        self._init_sensors(sensor_pins_list)
 
-    def _init_sensors(self):
-        for sensor, adress in iter(self._sensors_list):
+    def _init_sensors(self, sensor_pins_list: List[int]):
+        for adress, pin in enumerate(sensor_pins_list):
+            sensor = DistanceSensor(self._i2c, DigitalOutputDevice(pin))
             sensor.set_address(0x30 + adress)
+            self._sensors_list.append(sensor)
 
     def __call__(self):
         return [sensor() for sensor in self._sensors_list]
 
 
 if __name__ == "__main__":
-    i2c = board.I2C()
-    array = SensorArray([DistanceSensor(i2c, 25), DistanceSensor(i2c, 11), DistanceSensor(i2c, 1)])
+    array = SensorArray([25, 12, 1])
 
     print(array())
