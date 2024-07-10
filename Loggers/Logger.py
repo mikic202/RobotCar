ec354f67a1a590e21cb12a2fd19238944305b1f0
mikic202 <mikolaj.chomanski@gmail.com>
2024-07-10 20:14:01 +0200

    Add abstract logger class

---
+++ Loggers/Logger.py
@@ -0,0 +1,15 @@
+from ABC import ABC, abstractmethod
+from typing import Dict, Any
+
+class Logger(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def log(self, message: str):
+        pass
+
+    @abstractmethod
+    def log(self, data: Dict[str, Any]):
+        pass
+
