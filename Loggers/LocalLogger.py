eac5eb29b6b0f5f8ad658af66d62f3070dd9b703
mikic202 <mikolaj.chomanski@gmail.com>
2024-07-10 20:23:10 +0200

    Add local logging

---
+++ Loggers/LocalLogger.py
@@ -0,0 +1,15 @@
+from Loggers.Logger import Logger
+from typing import Dict, Any
+import logging
+import json
+
+class LocalLogger(Logger):
+    def __init__(self, file_path: str):
+        self._file_path = file_path
+        self._logger = logging.getLogger(self._file_path)
+
+    def log(self, message: str):
+        self._logger.info(message)
+
+    def log(self, data: Dict[str, Any]):
+        self._logger.info(json.dumps(data))
