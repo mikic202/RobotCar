b9d9dc27381c1bf053e68120012e408963b569f9
mikic202 <mikolaj.chomanski@gmail.com>
2024-07-19 17:18:42 +0200

    Add remote logger

---
+++ Loggers/RemoteLogger.py
@@ -0,0 +1,16 @@
+import socket
+from Loggers.Logger import Logger
+
+
+class RemoteLogger(Logger):
+    def __init__(self, host: str, port: int):
+        self._host = host
+        self._port = port
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._connection = self._socket.connect((self._host, self._port))
+
+    def log(self, message: str):
+        self._socket.sendall(message.encode())
+
+    def log(self, data: dict):
+        self._socket.sendall(str(data).encode())
