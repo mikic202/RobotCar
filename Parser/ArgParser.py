f1d87ae7a6f713e9aa0b942242daa64c47be0728
mikic202 <mikolaj.chomanski@gmail.com>
2024-08-12 19:24:42 +0200

    add basic argparser

--- Parser/ArgParser.py
+++ Parser/ArgParser.py
@@ -1,5 +1,44 @@
 import argparse
+from Loggers.RemoteLogger import RemoteLogger
+from Loggers.LocalLogger import LocalLogger
+from Loggers.Logger import Logger
+from Timers.Timer import Timer
+from Regulators.Regulator import Regulator
+from Robot.DifferentialRobot import DifferentialRobot
+from Robot.Robot import Robot
+from Regulators.PID import PID
+from HardwareClasses.SensorArray import SensorArray
+from HardwareClasses.MotorDrive import MotorDrive
+from HardwareClasses.Motor import Motor
 
 parser = argparse.ArgumentParser(description='Parse robot arguments')
 
-parser.add_argument('--logger', ['l', 'r'], help="Type of logger used by robot")
+parser.add_argument('--logger', choices=['l', 'r'], help="Type of logger used by robot", default='l')
+parser.add_argument('--Tp', type=float, help="Timer period for regulation loop", default=0.5)
+parser.add_argument('--robot', choices=['diff', 'sing'], help="Type of robot used", default='diff')
+parser.add_argument('--regulator', choices=['PID', 'human', 'DMC', 'NN'], help="Type of regulator used by robot", default='PID')
+
+
+def init_robot_from_args(args: argparse.Namespace) -> Robot:
+    motor_drive = MotorDrive([Motor(14, 15), Motor(23, 24)])
+    array = SensorArray([(5, -90), (25, -45), (12, 0), (1, 45), (21, 90)])
+
+    sensor_logger: Logger
+    control_logger: Logger
+    if args.logger == 'r':
+        sensor_logger = RemoteLogger()
+        control_logger = RemoteLogger()
+    else:
+        sensor_logger = LocalLogger("sensor.log")
+        control_logger = LocalLogger("control.log")
+
+    timer = Timer(args.Tp)
+
+    regulator: Regulator
+
+    if args.regulator == 'PID':
+        regulator = PID(6, 10, 5, args.Tp, 1, [150])
+
+    if args.robot == 'diff':
+        return DifferentialRobot(motor_drive, array, sensor_logger, control_logger, regulator, timer)
+
--- main.py
+++ main.py
@@ -1,32 +1,10 @@
-from HardwareClasses.SensorArray import SensorArray
-from Loggers.RemoteLogger import RemoteLogger
-from Loggers.LocalLogger import LocalLogger
-from HardwareClasses.MotorDrive import MotorDrive
-from Robot.DifferentialRobot import DifferentialRobot
-from HardwareClasses.Motor import Motor
-from Regulators.RemoteRegulator import RemoteRegulator
-from Regulators.PID import PID
-from Timers.Timer import Timer
-import time
+from Parser.ArgParser import init_robot_from_args, parser
 
 
 if __name__ == "__main__":
     try:
-        motor_drive = MotorDrive([Motor(14, 15), Motor(23, 24)])
-        array = SensorArray([(5, -90), (25, -45), (12, 0), (1, 45), (21, 90)])
-        print("Sensors established")
-        # sensor_logger = RemoteLogger("192.168.0.164", 65432)
-        # time.sleep(0.5)
-        # controll_logger = RemoteLogger("192.168.0.164", 65433)
-        sensor_logger = LocalLogger("sensor.txt")
-        time.sleep(0.5)
-        controll_logger = LocalLogger("contr.txt")
-        print("Loggers established")
-        regulator = PID(6, 10, 5, 0.5, 1, [150])
-        robot = DifferentialRobot(
-            motor_drive, array, sensor_logger, controll_logger, regulator, Timer(0.5)
-        )
+        robot = init_robot_from_args(parser.parse_args())
         robot()
     finally:
-        array.reset_addresses()
-    array.reset_addresses()
+        robot._sensor_array.reset_addresses()
+    robot._sensor_array.reset_addresses()
