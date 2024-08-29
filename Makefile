be98d7b780a8138ecaee34f9b91eb919d0f3b7c1
mikic202 <mikolaj.chomanski@gmail.com>
2024-08-29 19:42:00 +0200

    Fix sensor testing

--- Makefile
+++ Makefile
@@ -11,3 +11,9 @@ mypy:
 
 run:
 	@python3 -m main --Tp $(TIMER_PERIOD)  --logger $(LOGGER) --robot $(ROBOT) --regulator $(REGULATOR) --reg_args $(REG_ARGS)
+
+test_sensors:
+	@python3 -m test_sensors
+
+test_motors:
+	@python3 -m test_motors
--- test_sensors.py
+++ test_sensors.py
@@ -1,9 +1,11 @@
 from HardwareClasses.SensorArray import SensorArray
+from time import sleep
 
 if __name__ == "__main__":
     try:
         array = SensorArray([(5, -90), (25, -45), (12, 0), (1, 45), (21, 90)])
         for _ in range(5):
+            sleep(1)
             print(f"Sensor values: {array()}")
     finally:
         array.reset_addresses()
