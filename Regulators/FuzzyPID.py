6513f66b0bf12f34af3488578c97f294db9a4eb4
mikic202 <mikolaj.chomanski@gmail.com>
2024-10-23 21:24:03 +0200

    add trapeizodal fuzzy func

--- Regulators/FuzzyPID.py
+++ Regulators/FuzzyPID.py
@@ -1,5 +1,6 @@
 from Regulators.PID import PID
 import json
+import numpy as np
 
 
 class FuzzyPID(PID):
@@ -29,11 +30,25 @@ class FuzzyPID(PID):
             K_params.append(pid_params["K"])
             Ti_params.append(pid_params["Ti"])
             Td_params.append(pid_params["Td"])
-            self._fuzzy_functions.append(pid_params["fuzzy_functions"])
+            self._fuzzy_functions.append([some_fuzzy_function, pid_params["fuzzy_functions"]])
         return K_params, Ti_params, Td_params, setpoints
 
     def get_controll(self, input: list):
         raw_controll_output = super().get_controll(input)
         controll_output = []
-        for controll_value, fuzzy_functions in zip(raw_controll_output, self._fuzzy_functions):
-            controll_output.append(self.calculate_fuzzy_output(controll_value, fuzzy_functions))
+        for controll_value, fuzzy_functions in zip(input, self._fuzzy_functions):
+            controll_output.append(fuzzy_functions[0](controll_value, fuzzy_functions[1]))
+        return controll_output
+
+
+def some_fuzzy_function(x, func_params: list[float]):
+    func_params = sorted(func_params)
+    if x <= func_params[0] or x >= func_params[-1]:
+        return 0
+    if x >= func_params[1] and x <= func_params[2]:
+        return x
+    if x < func_params[1]:
+        coefficients = np.polyfit([func_params[0], func_params[1]], [0, 1], 1)
+        return np.polyval(coefficients, x)
+    coefficients = np.polyfit([func_params[2], func_params[3]], [1, 0], 1)
+    return np.polyval(coefficients, x)
