30f73637e98d57922138d531333f5fec88e15465
mikic202 <mikolaj.chomanski@gmail.com>
2024-10-24 21:00:52 +0200

    Change code to use new membership functions

--- Regulators/FuzzyPID.py
+++ Regulators/FuzzyPID.py
@@ -1,4 +1,6 @@
 from Regulators.PID import PID
+from Regulators.MembershipFunctions.TrapeizodalMembershipFunction import TrapeizodalMembershipFunction
+from Regulators.MembershipFunctions.MembershipFunction import MembershipFunction
 import json
 import numpy as np
 
@@ -7,7 +9,7 @@ class FuzzyPID(PID):
     def __init__(self, param_json_file: str, Tp: float = 1):
         self._param_file = param_json_file
         self._Tp = Tp
-        self._fuzzy_functions = []
+        self._fuzzy_functions: list[MembershipFunction] = []
         self.load_params_from_json(param_json_file)
 
     def load_params_from_json(self, param_json_file: str):
@@ -30,7 +32,7 @@ class FuzzyPID(PID):
             K_params.append(pid_params["K"])
             Ti_params.append(pid_params["Ti"])
             Td_params.append(pid_params["Td"])
-            self._fuzzy_functions.append([some_fuzzy_function, pid_params["fuzzy_functions"]])
+            self._fuzzy_functions.append(TrapeizodalMembershipFunction(pid_params["fuzzy_functions"]))
         return K_params, Ti_params, Td_params, setpoints
 
     def get_controll(self, input: list):
@@ -38,18 +40,5 @@ class FuzzyPID(PID):
         raw_controll_output = super().get_controll(input)
         controll_output = []
         for controll_value, fuzzy_functions in zip(raw_controll_output, self._fuzzy_functions):
-            controll_output.append(fuzzy_functions[0](controll_value, fuzzy_functions[1]))
+            controll_output.append(fuzzy_functions(controll_value))
         return sum(controll_output)
-
-
-def some_fuzzy_function(x, func_params: list[float]):
-    func_params = sorted(func_params)
-    if x <= func_params[0] or x >= func_params[-1]:
-        return 0
-    if x >= func_params[1] and x <= func_params[2]:
-        return x
-    if x < func_params[1]:
-        coefficients = np.polyfit([func_params[0], func_params[1]], [0, 1], 1)
-        return np.polyval(coefficients, x)
-    coefficients = np.polyfit([func_params[2], func_params[3]], [1, 0], 1)
-    return np.polyval(coefficients, x)
