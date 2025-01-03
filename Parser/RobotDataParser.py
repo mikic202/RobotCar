c0d32fe3cc00a3ebd91f05d9b165e964180abcc0
mikic202 <mikolaj.chomanski@gmail.com>
2025-01-03 20:53:31 +0100

    Add typing for parsers

--- Parser/RobotDataParser.py
+++ Parser/RobotDataParser.py
@@ -3,11 +3,11 @@ from typing import Any
 
 class RobotDataParser:
     @staticmethod
-    def convert_sensor_data_to_dict(data):
+    def convert_sensor_data_to_dict(data: list[tuple[float, float]]) -> list[dict[str, Any]]:
         return [{"angle": reading[1], "value": reading[0]} for reading in data]
 
     @staticmethod
-    def convert_motor_data_to_dict(data):
+    def convert_motor_data_to_dict(data: list[float]) -> list[dict[str, Any]]:
         return [
             {"control_name": motor, "value": value} for motor, value in enumerate(data)
         ]
@@ -15,7 +15,7 @@ class RobotDataParser:
     @staticmethod
     def convert_data_dict_to_csv_rovs(
         log_values: list[dict[str, Any]], sample_index: int
-    ):
+    ) -> list[list[Any]]:
         return [
             [*[value for value in reading.values()], sample_index]
             for reading in log_values
